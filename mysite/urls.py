db8ce397cdd269b861a9edb4c2d7da4ffcb0142e
Steve <steve.l.chen@gmail.com>
2015-12-06 22:51:47 -0500

    able to now save new posts and view images

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     # redirect from index to blog.urls
     url(r'', include('blog.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
